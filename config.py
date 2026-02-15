5f5e4c769d697a38f97429d33630535f14a3cdca
yuktirapartiwar <1599.yukti@gmail.com>
2026-02-15 17:44:27 +0530

    Added configuration management using environment variables

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
