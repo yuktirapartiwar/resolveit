246e6a7112e23d917fc56edc583927a7448dfdf3
yuktirapartiwar <1599.yukti@gmail.com>
2026-02-15 18:01:54 +0530

    Initialized Flask app with SQLAlchemy and JWT

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from config import Config
+
+db = SQLAlchemy()
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    jwt.init_app(app)
+
+    from app.routes import main
+    app.register_blueprint(main)
+
+    return app
