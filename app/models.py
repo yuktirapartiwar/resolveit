e41f9a7549f3a5bbbad2a87a6bb9d74be4727ec0
yuktirapartiwar <1599.yukti@gmail.com>
2026-02-15 18:17:23 +0530

    implemented User model

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from app import db
+from datetime import datetime
+import uuid
+
+class User(db.Model):
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
