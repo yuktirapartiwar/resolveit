fa569246586ac8c0c5ca814139c324b5931421eb
yuktirapartiwar <1599.yukti@gmail.com>
2026-02-21 10:03:58 +0530

    Added Ticket model

--- app/models.py
+++ app/models.py
@@ -9,3 +9,16 @@ class User(db.Model):
     password_hash = db.Column(db.String(200), nullable=False)
     role = db.Column(db.String(20), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Ticket(db.Model):
+    __tablename__ = "tickets"
+
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    status = db.Column(db.String(20), default="OPEN")
+
+    created_by = db.Column(db.String(36), db.ForeignKey("user.id"), nullable=False)
+    assigned_to = db.Column(db.String(36), db.ForeignKey("user.id"), nullable=True)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
